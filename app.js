9636b0cd07daa986a8d95be34576c5b9a8c2bb37
Chinwendu Agbaetuo <silviachinwendu@gmail.com>
2022-05-18 23:19:27 +0100

    install concurrently package

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('<strong>Hello World!</strong>')
-})
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+  // res.render("index", {
+  //   meta: {
+  //     data: {
+  //       title: "Floema",
+  //       description: "Metadata description",
+  //     },
+  //   },
+  // });
+  
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
