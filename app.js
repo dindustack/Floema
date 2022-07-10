58521ab063bfd40ba2bc11a825a1130ce38c21c6
Chinwendu Agbaetuo <silviachinwendu@gmail.com>
2022-07-10 16:11:03 +0100

    partial fix

--- app.js
+++ app.js
@@ -1,7 +1,50 @@
+require('dotenv').config()
+
+const fetch = require('node-fetch');
 const express = require("express");
 const app = express();
 const path = require("path");
 const port = 3000;
+
+const Prismic = require('@prismicio/client')
+const PrismicH = require('@prismicio/helpers')
+
+// Initialize the prismic.io api
+const initApi = (req) => {
+  return Prismic.createClient(process.env.PRISMIC_ENDPOINT, {
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+    req,
+    fetch,
+  });
+};
+
+// Link Resolver
+const handleLinkResolver = (doc) => {
+  if (doc.type === 'product') {
+    return `/detail/${doc.slug}`;
+  }
+
+  if (doc.type === 'collections') {
+    return '/collections';
+  }
+
+  if (doc.type === 'about') {
+    return `/about`;
+  }
+
+  // Default to homepage
+  return '/';
+};
+
+
+app.use((req, res, next) => {
+  res.locals.ctx = {
+    PrismicH,
+    endpoint: process.env.PRISMIC_ENDPOINT,
+    linkResolver: handleLinkResolver,
+  }
+  next()
+})
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
@@ -15,20 +58,25 @@ app.get("/", (req, res) => {
   //     },
   //   },
   // });
-  res.render('pages/home')
+  res.render("pages/home");
 });
 
-app.get('/about', (req, res) => {
-  res.render('pages/about')
-})
+app.get('/about', async (req, res) => {
+  const api = await initApi(req);
+  const defaults = await handleRequest(api);
 
-app.get('/detail/:uid', (req, res) => {
-  res.render('pages/detail')
-})
+  res.render('pages/about', {
+    ...defaults,
+  });
+});
 
-app.get('/collections', (req, res) => {
-  res.render('pages/collections')
-})
+app.get("/detail/:uid", (req, res) => {
+  res.render("pages/detail");
+});
+
+app.get("/collections", (req, res) => {
+  res.render("pages/collections");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
