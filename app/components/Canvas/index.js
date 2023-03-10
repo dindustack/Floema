7a4cfa9dae2f7ac3e666a500db3067c9f34c04a3
Chinwendu Agbaetuo <silviachinwendu@gmail.com>
2023-03-10 19:31:27 +0100

    create camera fix

--- app/components/Canvas/index.js
+++ app/components/Canvas/index.js
@@ -1,18 +1,26 @@
-import { Camera, Renderer, Transform } from "ogl";
+import { Box, Camera, Mesh, Program, Renderer, Transform } from "ogl";
 
 export default class Canvas {
   constructor() {
     this.createRenderer();
   }
 
-  createRenderer () {
-    this.renderer = new Renderer()
+  createRenderer() {
+    this.renderer = new Renderer();
 
-    this.gl = this.renderer.gl
-    document.body.appendChild(this.gl.canvas)
+    this.gl = this.renderer.gl;
+    document.body.appendChild(this.gl.canvas);
+  }
+
+  createCamera() {
+    this.camera = new Camera(gl);
+    this.camera.position.z = 5;
   }
 
   onResize() {
-    this.renderer.setSize(window.innerWidth, window.innerHeight)
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.camera.perspective({
+      aspect: window.innerWidth / window.innerHeight,
+    });
   }
 }
