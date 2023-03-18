4d6834d447ec05a1648e2700afdeee909dd6f335
Chinwendu Agbaetuo <silviachinwendu@gmail.com>
2023-03-18 17:52:04 +0100

    commit

--- app/components/Canvas/Home.js
+++ app/components/Canvas/Home.js
@@ -1,13 +1,19 @@
+import {Plane} from 'ogl'
 import map from "lodash/map"
 
 import Media from "./Media"
 
 export default class {
   constructor ({gl}) {
+    this.gl = gl
     this.medias = document.querySelectorAll(".home__gallery__media__image")
 
     this.createGeometry()
     this.createGallery()
+  }
+
+  createGeometry() {
+    this.geometry = new Plane(this.gl)
   }
 
   createGallery() {
